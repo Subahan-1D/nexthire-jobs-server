c9c503af9d2c88f7988e1949e35862e18ac8cb1d
subahan ali <subahanislam523@gmail.com>
2025-03-11 00:42:39 +0600

    added to by filter implement

--- index.js
+++ index.js
@@ -179,10 +179,13 @@ async function run() {
 
     // get jobs data for pagination
     app.get("/all-jobs", async (req, res) => {
-      const size = parseInt(req.query.size)  ;
-      const page = parseInt(req.query.page) -1;
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const filter = req.query.filter;
+      let query = {};
+      if (filter) query = { category: filter };
       const result = await jobsCollection
-        .find()
+        .find(query)
         .skip(page * size)
         .limit(size)
         .toArray();
